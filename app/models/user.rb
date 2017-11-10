5249106dad176c06199189ba494c35fe67dde35a
osamuhiraishi <osm890221@gmail.com>
2017-11-10 16:00:44 +0000

    Add password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,19 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
   before_save   :downcase_email
   before_create :create_activation_digest
-  validates :name,  presence: true, length: { maximum:  50 }
 
+
+  validates :name,  presence: true, length: { maximum:  50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-  
+  validates :password, presence: true, 
+    length: { minimum: 6 }, allow_nil: true
+
+
   def User.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
@@ -31,16 +34,17 @@ class User < ApplicationRecord
     self.update_attribute(:remember_digest, nil)
   end
   
-  # トークンがダイジェストと一致したらtrueを返す
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
   def authenticated?(attribute, token)
-    digest = send("#{attribute}_digest")
+    digest = self.send("#{attribute}_digest")
     return false if digest.nil?
     BCrypt::Password.new(digest).is_password?(token)
   end
   
-    # アカウントを有効にする
+  # アカウントを有効にする
   def activate
-    update_columns(activated: true, activated_at: Time.zone.now)
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
   end
   
   # 有効化用のメールを送信する
@@ -48,18 +52,33 @@ class User < ApplicationRecord
     UserMailer.account_activation(self).deliver_now
   end
   
-  private
-  
-  # メールアドレスをすべて小文字にする
-  def downcase_email
-    self.email = email.downcase
+  # パスワード再設定の属性を設定する
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
   end
   
-  # 有効化トークンとダイジェストを有効化および代入する
-  def create_activation_digest
-    self.activation_token = User.new_token
-    self.activation_digest = User.digest(activation_token)
+  # パスワード再設定用メールを送信する
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
   end
   
+  # パスワード再設定の期限が切れている場合はtrueを返す
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      self.email = self.email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(self.activation_token)
+      # @user.activation_digest => ハッシュ値
+    end
 end
